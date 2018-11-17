d95d2b5298367b6f6459eab880c1136f5fa576dc
Masaharu S <30203831+natsuyoshi-jr@users.noreply.github.com>
2018-11-18 00:02:06 +0900

    commit python script for formatting json string.

---
+++ python3_format_json.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3.6
+# coding: utf-8
+import sys
+import json
+
+# パイプで渡された値を受け取る
+standard_output_value = sys.stdin.readline()
+
+# 文字列がjson形式かチェック
+try:
+    # 文字列をjsonデコード
+    json_string = json.loads(standard_output_value)
+except ValueError:
+    # 文字列がjson形式でないことをターミナルに出力
+    sys.stdout.write("No JSON object could be decoded")
+    # エラーコードを返し処理を終了
+    sys.exit(1)
+
+# jsonを整形する
+# ensure_asciiをFalseにすることで非asciiに対応させる
+formatted_json = json.dumps(json_string,
+                            ensure_ascii=False,
+                            indent=4,
+                            separators=(',', ': '))
+# ターミナルに整形したjsonを出力する
+sys.stdout.write(formatted_json)
+# 正常コードを返し処理を終了
+sys.exit(0)
